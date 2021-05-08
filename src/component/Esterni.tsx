64510db5a39d0bbe84e3557b2b2ac4b20046cee1
Alessandro <alessandrobacci142@gmail.com>
2021-05-08 20:31:49 +0200

    new photos

--- src/component/Esterni.tsx
+++ src/component/Esterni.tsx
@@ -1,107 +1,118 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 
-import cortile1 from '../images/casa/esterni/cortile1.jpg';
-import esterno1 from '../images/casa/esterni/esterno1.jpeg';
-import esterno2 from '../images/casa/esterni/esterno2.jpg';
-import esterno3 from '../images/casa/esterni/esterno3.jpeg';
-import piantina from '../images/casa/esterni/piantina.png';
-import postoauto from '../images/casa/esterni/postoauto.jpg';
-import vista1 from '../images/casa/esterni/vista1.jpg';
-import vista2 from '../images/casa/esterni/vista2.jpg';
-import vista3 from '../images/casa/esterni/vista3.jpg';
-import vista4 from '../images/casa/esterni/vista4.jpeg';
+import cortile1 from "../images/casa/esterni/cortile1.jpg";
+import esterno1 from "../images/casa/esterni/esterno1.jpeg";
+import esterno3 from "../images/casa/esterni/esterno3.jpeg";
+import esterno4 from "../images/casa/esterni/esterno4.jpeg";
+import esterno5 from "../images/casa/esterni/esterno5.jpeg";
+import esterno6 from "../images/casa/esterni/esterno6.jpeg";
+
+import postoauto from "../images/casa/esterni/postoauto.jpg";
+import vista1 from "../images/casa/esterni/vista1.jpg";
+import vista3 from "../images/casa/esterni/vista3.jpg";
+import vista5 from "../images/casa/esterni/vista5.jpeg";
+import vista6 from "../images/casa/esterni/vista6.jpeg";
+import vista7 from "../images/casa/esterni/vista7.jpeg";
 const fotoEsterni = [
-    {
-        src: cortile1,
-        alt: "Foto cortile",
-        caption: "Cortile"
-    },
-    {
-        src: esterno1,
-        alt: "Foto esterno",
-        caption: "Esterno"
-    },
-    {
-        src: esterno2,
-        alt: "Foto esterno",
-        caption: "Esterno"
-    },
-    {
-        src: esterno3,
-        alt: "Foto esterno",
-        caption: "Esterno"
-    },
-    
-    {
-        src: piantina,
-        alt: "Foto piantina",
-        caption: "Piantina"
-    },
-    {
-        src: postoauto,
-        alt: "Foto posto auto",
-        caption: "Posto auto"
-    },
-    {
-        src: vista1,
-        alt: "Foto vista mare",
-        caption: "Vista mare"
-    },
-    // {
-    //     src: vista2,
-    //     alt: "Foto vista mare",
-    //     caption: "Vista mare"
-    // },
-    // {
-    //     src: vista4,
-    //     alt: "Foto vista mare",
-    //     caption: "Vista mare"
-    // },
-    {
-        src: vista3,
-        alt: "Foto vista mare",
-        caption: "Vista mare"
-    }
+  {
+    src: esterno1,
+    alt: "Foto esterno",
+    caption: "Esterno",
+  },
+  {
+    src: cortile1,
+    alt: "Foto cortile",
+    caption: "Cortile",
+  },
+  {
+    src: vista1,
+    alt: "Foto vista mare",
+    caption: "Vista mare",
+  },
+  {
+    src: vista6,
+    alt: "Foto vista mare",
+    caption: "Vista mare",
+  },
+  {
+    src: vista5,
+    alt: "Foto vista mare",
+    caption: "Vista mare",
+  },
+  {
+    src: esterno5,
+    alt: "Foto esterno",
+    caption: "Esterno",
+  },
+  {
+    src: vista7,
+    alt: "Foto vista mare",
+    caption: "Vista mare",
+  },
+  {
+    src: esterno6,
+    alt: "Foto esterno",
+    caption: "Esterno",
+  },
+  {
+    src: esterno4,
+    alt: "Foto esterno",
+    caption: "Esterno",
+  },
+
+  {
+    src: esterno3,
+    alt: "Foto esterno",
+    caption: "Esterno",
+  },
+
+  {
+    src: postoauto,
+    alt: "Foto posto auto",
+    caption: "Posto auto",
+  },
+  {
+    src: vista3,
+    alt: "Foto vista mare",
+    caption: "Vista mare",
+  },
 ];
 
 const slideShow = fotoEsterni.map((el, key) => {
-    return (
-        <div key = {key}>
-        <img
-            className = "w-100"
-            src = {el.src}
-            alt = {el.alt}
+  return (
+    <div key={key}>
+      <img className="w-100" src={el.src} alt={el.alt} />
+      <p>
+        <h2 style={{ color: "white" }}> {el.caption}</h2>
+      </p>
+    </div>
+  );
+});
 
-        />
-        <p>
-            <h2 style = {{color: "white"}}> {el.caption}</h2>
-        </p>
-        </div>
-    );
-})
-
-function Esterni (props: any) {
-    return (
-        <Container fluid>
-            <Row className = "justify-content-center mt-3">
-                <Col className = "text-center"> <h2 className = "align-self-center"> Esterni</h2></Col>
-            </Row>
-            <Row className = "justify-content-center mt-5">
-                <Col></Col>
-                <Col> 
-                <Carousel dynamicHeight = {false} showThumbs = {true} infiniteLoop = {true}>
-                    {slideShow}
-                </Carousel>
-                </Col>
-                <Col></Col>
-            </Row>
-        </Container>
-        
-    );
+function Esterni(props: any) {
+  return (
+    <Container fluid>
+      <Row className="justify-content-center mt-3">
+        <Col className="text-center">
+          {" "}
+          <h2 className="align-self-center"> Esterni</h2>
+        </Col>
+      </Row>
+      <Row className="justify-content-center mt-5">
+        <Col></Col>
+        <Col>
+          <Carousel dynamicHeight={false} showThumbs={true} infiniteLoop={true}>
+            {slideShow}
+          </Carousel>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Esterni;
--- src/component/Interni.tsx
+++ src/component/Interni.tsx
@@ -1,69 +1,91 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 
-import bagno1 from '../images/casa/interni/bagno1.jpg';
-import camera from '../images/casa/interni/camera.jpg';
-import sala1 from '../images/casa/interni/sala.jpg';
-import sala2 from '../images/casa/interni/sala2.jpg';
+import bagno1 from "../images/casa/interni/bagno1.jpg";
+import bagno2 from "../images/casa/interni/bagno2.jpeg";
+import bagno3 from "../images/casa/interni/bagno3.jpeg";
 
+import camera from "../images/casa/interni/camera.jpg";
+import camera2 from "../images/casa/interni/camera2.jpeg";
+
+import sala1 from "../images/casa/interni/sala.jpg";
+import sala2 from "../images/casa/interni/sala2.jpg";
+import sala3 from "../images/casa/interni/sala3.jpeg";
 
 const fotoInterni = [
-    {
-        src: bagno1,
-        alt: "Foto bagno",
-        caption:"Bagno"
-    },
-    {
-        src: camera,
-        alt: "Foto camera",
-        caption: "Camera da letto"
-    },
-    {
-        src: sala1,
-        alt: "Foto sala",
-        caption: "Sala da pranzo"
-    },
-    {
-        src: sala2,
-        alt: "Foto sala 2",
-        caption: "Sala da pranzo"
-    }
+  {
+    src: bagno1,
+    alt: "Foto bagno",
+    caption: "Bagno",
+  },
+  {
+    src: bagno2,
+    alt: "Foto bagno",
+    caption: "Bagno",
+  },
+  {
+    src: bagno3,
+    alt: "Foto bagno",
+    caption: "Bagno",
+  },
+  {
+    src: camera,
+    alt: "Foto camera",
+    caption: "Camera da letto",
+  },
+  {
+    src: camera2,
+    alt: "Foto camera",
+    caption: "Camera da letto",
+  },
+  {
+    src: sala1,
+    alt: "Foto sala",
+    caption: "Sala da pranzo",
+  },
+  {
+    src: sala2,
+    alt: "Foto sala 2",
+    caption: "Sala da pranzo",
+  },
+  {
+    src: sala3,
+    alt: "Foto sala 3",
+    caption: "Sala da pranzo",
+  },
 ];
 
 const slideShow = fotoInterni.map((el, key) => {
-    return (
-    <div key = {key}>
-        <img
-            className = "w-100"
-            src = {el.src}
-            alt = {el.alt}
+  return (
+    <div key={key}>
+      <img className="w-100" src={el.src} alt={el.alt} />
+    </div>
+  );
+});
 
-        />
-        
-    </div>);
-})
-
-function Interni (props: any) {
-    return (
-        <Container fluid>
-            <Row className = "justify-content-center mt-3">
-                <Col className = "text-center"> <h2 className = "align-self-center"> Interni</h2></Col>
-            </Row>
-            <Row className = "justify-content-center mt-5">
-                <Col></Col>
-                <Col> 
-                <Carousel dynamicHeight = {true} showThumbs = {true} infiniteLoop = {true}>
-                    {slideShow}
-                </Carousel>
-                </Col>
-                <Col></Col>
-            </Row>
-        </Container>
-        
-    );
+function Interni(props: any) {
+  return (
+    <Container fluid>
+      <Row className="justify-content-center mt-3">
+        <Col className="text-center">
+          {" "}
+          <h2 className="align-self-center"> Interni</h2>
+        </Col>
+      </Row>
+      <Row className="justify-content-center mt-5">
+        <Col></Col>
+        <Col>
+          <Carousel dynamicHeight={true} showThumbs={true} infiniteLoop={true}>
+            {slideShow}
+          </Carousel>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Interni;
